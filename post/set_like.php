496d2f36be922e651e576072e7d7062dd911567b
quent77np <andrieuquentin8@gmail.com>
2018-11-06 16:33:28 +0100

    modif query in set_like

--- post/set_like.php
+++ post/set_like.php
@@ -16,7 +16,7 @@
 
             if (!empty($id_user) && !empty($id_user_chosen))
             {
-                $select = mysqli_query($db, "SELECT * FROM match_request WHERE id_userSend=".$id_user." AND id_userMatch=".$id_user_chosen.";";
+                $select = mysqli_query($db, "SELECT * FROM match_request WHERE id_userSend=".$id_user." AND id_userMatch=".$id_user_chosen.";");
 
                 if (mysqli_num_rows($select)==1)
                 {
