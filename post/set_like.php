557845082bf7c373225cfa33fadebf6d0f078fa9
Brage <brage.skjonborg@hotmail.no>
2018-11-10 02:47:28 +0100

    Altered set_like

--- post/set_like.php
+++ post/set_like.php
@@ -21,9 +21,10 @@
                 if (mysqli_num_rows($select)==1)
                 {
                     //Its a match
-
-                    $sql = "UPDATE match_request SET requestState=1 WHERE id_userSend=".$id_user."  AND id_userMatch=".$id_user_chosen.";";
-                    $sql1 = " INSERT INTO tandem (`id_tandem`,`id_user1`, `id_user2`, `conversationName`, `delete_conversation_user`,`delete_conversation_user2`) VALUES ( NULL ,".$id_user.",".$id_user_chosen.", NULL , NULL , NULL);";
+                    //Swapped id to make the reverse lookup to update exisint request since user posting approve has a pending request
+                    $sql = "UPDATE match_request SET requestState=1 WHERE id_userSend=".$id_user_chosen."  AND id_userMatch=".$id_user.";";
+                    //Removed id_tandem from query since it's handled by database (AUTO_INCREMENT)
+                    $sql1 = " INSERT INTO tandem (`id_user1`, `id_user2`, `conversationName`, `delete_conversation_user`,`delete_conversation_user2`) VALUES (".$id_user.",".$id_user_chosen.", NULL , NULL , NULL);";
                     mysqli_query($db,$sql);
                     mysqli_query($db,$sql1);
 
